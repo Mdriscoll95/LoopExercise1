6b9112b52da710f10fa5a11f1df86583a31e3d60
Michael Driscoll <michael.driscoll951@gmail.com>
2023-05-17 09:29:16 -0700

    hw

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 0;i < 5; i++)
+        {
+        Console.WriteLine($"{i}");
+
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+    }
+}
